07643991a8a935c021a00a2dcdf417572806b1ea
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-10-19 14:34:34 +0600

    use filering and search system for data fetching

--- src/components/Sections/Books.jsx
+++ src/components/Sections/Books.jsx
@@ -1,39 +1,74 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useState } from "react";
 import Book_Card from "../Cards/Book_Card";
 import Loading from "../Shared/Loading";
+import Empty from "../Shared/Empty";
 
 const Books = () => {
-  const [search,setSearch] = useState("");
-  const [select,setSelect] = useState("");
-  const { data: Books = [], isLoading } = useQuery({
-    queryKey: ["books"],
-    queryFn: async () => {
-      const { data } = await axios.get("https://gutendex.com/books");
-      return data.results;
-    },
+  const [search, setSearch] = useState("");
+  const [select, setSelect] = useState("");
+
+  const [forFilter, items] = useQueries({
+    queries: [
+      {
+        queryKey: ["forFilter"],
+        queryFn: async () => {
+          const { data } = await axios.get("https://gutendex.com/books");
+          return data.results;
+        },
+      },
+      {
+        queryKey: ["books", search, select],
+        queryFn: async () => {
+          const { data } = await axios.get(
+            `https://gutendex.com/books?search=${search}&topic=${select}`
+          );
+          return data.results;
+        },
+      },
+    ],
   });
 
-  console.log(Books);
+  //for filtering
+  const filtering = forFilter.data || [];
+  const books = items.data || [];
+
   return (
     <div>
-      <div>
+      <div className="flex justify-around py-4">
         <div>
-        <input type="text" className="border-2 w-full p-2" />
+          <input
+            type="text"
+            className="border-2 w-full p-2"
+            onChange={(event) => setSearch(event.target.value)}
+          />
         </div>
         <div>
-          <select>
-            <option></option>
+          <select
+            value={select}
+            onChange={(event) => setSelect(event.target.value)}
+            className="w-full p-2"
+          >
+            <option defaultValue="">Default</option>
+            {filtering?.map((book) =>
+              book?.bookshelves?.map((genre, idx) => (
+                <option key={idx}>{genre.replace("Browsing:", "")}</option>
+              ))
+            )}
           </select>
         </div>
       </div>
+      <div className="flex justify-center">
+        {items.isLoading && <Loading></Loading>}
+        {books.length === 0 && !items.isLoading && <Empty></Empty>}
+      </div>
       <div>
         <div className="flex justify-center">
-        {isLoading && <Loading></Loading>}
+          {books.isLoading && <Loading></Loading>}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-2">
-          {Books.map((book, idx) => (
+          {books.map((book, idx) => (
             <Book_Card key={idx} book={book}></Book_Card>
           ))}
         </div>
